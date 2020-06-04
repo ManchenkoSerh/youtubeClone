d4f007bfb5785b92bf183f0d18dde9e376f23ff6
Serhii Manchenko <grandx.go@gmail.com>
2020-06-04 20:00:42 +0300

    add alt to img tag in commentitem

--- src/components/comments-item/CommentsItem.js
+++ src/components/comments-item/CommentsItem.js
@@ -1,10 +1,10 @@
-import React, {Component, useEffect} from "react";
+import React from "react";
 
 const CommentsItem=({snippet})=>{
         console.log(snippet.topLevelComment.snippet)
         return(
             <div>
-                <img src={snippet.topLevelComment.snippet.authorProfileImageUrl}/>
+                <img src={snippet.topLevelComment.snippet.authorProfileImageUrl} alt='authorProfileImage'/>
                 <span>{snippet.topLevelComment.snippet.authorDisplayName}</span>
                 <p>{snippet.topLevelComment.snippet.textOriginal}</p>
             </div>
