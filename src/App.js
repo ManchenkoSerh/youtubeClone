d70809e2650adfea21f1fe6f6d375b3d1fe89499
ArtemChumanov <artem_chumanov@ukr.net>
2020-06-04 14:39:03 +0300

    edit App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import InputSearch from "./containers/input-search/InputSearch";
 function App() {
   return (
+
     <BrowserRouter>
     <div >
       <Grid padded >
